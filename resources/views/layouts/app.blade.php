07b6c369dbe91e20bd50d7b6e088b187ae4da102
harizfajar <harizfajarae@gmail.com>
2025-07-07 23:09:09 +0700

    cors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,18 +10,18 @@
     <meta name="author" content="">
 
     <title>Belajar - SPK AHP</title>
-    <link rel="icon" type="image/svg+xml" href="{{ secure_asset('template/img/undraw_rocket.svg') }}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('template/img/undraw_rocket.svg') }}">
 
 
     <!-- Custom fonts for this template-->
-    <link href="{{ secure_asset('template/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('template/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <!-- Custom styles for this template-->
-    <link href="{{ secure_asset('template/css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('template/css/sb-admin-2.min.css') }}" rel="stylesheet">
 
 </head>
 
